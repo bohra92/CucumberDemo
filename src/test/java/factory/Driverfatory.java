2ec901a09095147ef7d12f108acf927539970ed2
Bohra92 <bohratanuj@gmail.com>
2023-03-19 05:16:14 +0530

    fixed chromedriver 111 issue

--- src/test/java/factory/Driverfatory.java
+++ src/test/java/factory/Driverfatory.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,7 +14,7 @@ public class Driverfatory {
 
 	public WebDriver init_driver(String browser) {
 		WebDriverManager.chromedriver().setup();
-		tlDriver.set(new ChromeDriver());
+		tlDriver.set(new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*")));
 		getdriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		getdriver().manage().window().maximize();
 		return getdriver();
